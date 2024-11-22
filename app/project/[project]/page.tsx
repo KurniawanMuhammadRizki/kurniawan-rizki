de39b46a1ec0911b1363f7b33c41e35f485580d0
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-11-22 23:59:11 +0700

    feat(project) add: project data

--- app/project/[project]/page.tsx
+++ app/project/[project]/page.tsx
@@ -1,47 +1,30 @@
 "use client";
 
+import projectData from "@/types/project/ProjectData";
 import { useParams } from "next/navigation";
 import ProjectDetail from "./component/ProjectDetail";
 import ProjectHero from "./component/ProjectHero";
-import { SiGraphql, SiNextdotjs, SiSpring } from "react-icons/si";
 
 const Page = () => {
   const { project } = useParams<{ project: string }>();
 
-  // Data proyek
-  const projectData = {
-    title: "Rooms",
-    descriptionList: [
-      "Property rental platform developed collaboratively with a partner",
-      "Frontend: Next.js | Backend: Spring with GraphQL",
-      "Role: Full-stack developer, worked on both frontend and backend implementations",
-    ],
-    techStack: [
-      { name: "Next.js", icon: <SiNextdotjs /> },
-      { name: "Spring", icon: <SiSpring /> },
-      { name: "GraphQL", icon: <SiGraphql /> },
-    ],
-    carousel: {
-      projectsName: "rooms",
-      imageNumber: 6,
-    },
-    cta: {
-      description: "View Project",
-      link: "https://www.rooms.co.id",
-    },
-    hero: {
-      title: project,
-      subtitle: "Property Renting Website",
-      description:
-        "Property Renting Web App helps users compare accommodation prices by date, influenced by holidays and manually set special dates.",
-      imageUrl: "/projects/rooms/rooms-0.jpeg",
-    },
-  };
+  const currentProject = projectData[project as keyof typeof projectData];
+
+  if (!currentProject) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-semibold">Project not found</h1>
+        <p className="text-gray-600">
+          Please check the URL or try again later.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <ProjectHero {...projectData.hero} />
-      <ProjectDetail {...projectData} />
+      <ProjectHero {...currentProject.hero} />
+      <ProjectDetail {...currentProject} />
       <section className="w-full h-[500px]" data-nav-color="black"></section>
       <div className="h-10"></div>
     </div>
